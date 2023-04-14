8c1daaf014c18f4166e132477f978200173a4a4c
Matias Ariel Eguia <matias_eguia@hotmail.com>
2023-04-14 20:35:39 -0300

    modificacion

---
+++ src/ruleta.ts
@@ -0,0 +1,14 @@
+import { PlayGame } from "./playgame";
+
+export class Ruleta extends PlayGame{
+    protected premio: number;
+    
+    play() {
+        
+    }
+    getCash() {
+        
+    }
+
+}
+ 
